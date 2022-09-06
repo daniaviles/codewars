cc20b236b45cef34bdfd78eb602302aa80347aea
Dani <danielle.a.aviles@gmail.com>
2022-09-06 14:35:52 -0400

    Added 8kyu coding challenge Switch It Up

---
+++ js/kata11.js
@@ -0,0 +1,25 @@
+// Switch It Up
+
+// When provided with a number between 0-9, return it in words.
+
+// Input :: 1
+
+// Output :: "One".
+
+// If your language supports it, try using a switch statement.
+
+function switchItUp(number){
+    const nums = {
+      0: 'Zero',
+      1: 'One',
+      2: 'Two',
+      3: 'Three',
+      4: 'Four',
+      5: 'Five',
+      6: 'Six',
+      7: 'Seven',
+      8: 'Eight',
+      9: 'Nine'
+    };
+    return nums[number];
+  }
