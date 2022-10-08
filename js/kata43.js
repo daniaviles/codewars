d9c7e3ec89608d99ca4352f3a5e5f720846bc09f
Dani <danielle.a.aviles@gmail.com>
2022-10-08 17:43:03 -0400

    Added 7 kyu coding challenge Reverse Letter

---
+++ js/kata43.js
@@ -0,0 +1,25 @@
+//Simple Fun #176: Reverse Letter
+
+
+// Task
+
+// Given a string str, reverse it and omit all non-alphabetic characters.
+
+// Example
+
+// For str = "krishan", the output should be "nahsirk".
+
+// For str = "ultr53o?n", the output should be "nortlu".
+
+// Input/Output
+
+// [input] string str
+// A string consists of lowercase latin letters, digits and symbols.
+
+// [output] a string
+
+function reverseLetter(str) {
+    str = str.replace(/[^A-Za-z]/g, "")
+    str = str.split("").reverse()
+    return str.join("");
+  }
