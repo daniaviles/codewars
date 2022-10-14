ca63e33e0e50702c7737a4d692f84591b34eb9bd
Dani <danielle.a.aviles@gmail.com>
2022-10-14 18:12:54 -0400

    Added 8kyu coding challenge Exclamation Mark Series

---
+++ js/kata49.js
@@ -0,0 +1,18 @@
+//Exclamation marks series #11: Replace all vowel to exclamation mark in the sentence
+
+
+// Description:
+
+// Replace all vowel to exclamation mark in the sentence. aeiouAEIOU is vowel.
+
+// Examples
+
+// replace("Hi!") === "H!!"
+// replace("!Hi! Hi!") === "!H!! H!!"
+// replace("aeiou") === "!!!!!"
+// replace("ABCDE") === "!BCD!"
+
+function replace(s){
+    return s.replace(/[a,e,i,o,u,A,E,I,O,U]/g,"!")
+    
+  }
