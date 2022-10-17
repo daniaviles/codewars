ad02d8e3653588e744080e5a11079121b3824546
Dani <danielle.a.aviles@gmail.com>
2022-10-17 10:26:26 -0400

    Added 6kyu coding challenge Give Me a Diamond

---
+++ js/kata52.js
@@ -0,0 +1,54 @@
+// Jamie is a programmer, and James' girlfriend. She likes diamonds, and wants a diamond string from James. Since James doesn't know how to make this happen, he needs your help.
+
+// Task
+
+// You need to return a string that looks like a diamond shape when printed on the screen, using asterisk (*) characters. Trailing spaces should be removed, and every line must be terminated with a newline character (\n).
+
+// Return null/nil/None/... if the input is an even number or negative, as it is not possible to print a diamond of even or negative size.
+
+// Examples
+
+// A size 3 diamond:
+
+//  *
+// ***
+//  *
+// ...which would appear as a string of " *\n***\n *\n"
+
+// A size 5 diamond:
+
+//   *
+//  ***
+// *****
+//  ***
+//   *
+// ...that is:
+
+// "  *\n ***\n*****\n ***\n  *\n"
+
+function diamond(n){
+  
+    if(n < 0 || n % 2 === 0) return null;
+  
+  
+    // bottom diamond 
+    let bottom = "";
+    let bottomCount = 0;
+    for(let i = n; i > 0; i-=2){
+        let space = " ".repeat(bottomCount);
+        bottom += `${space}${`*`.repeat(i)}\n`;
+        bottomCount++;
+    }
+  
+    // top diamond
+    let top = "";
+    let topCount = bottomCount-1;
+  
+    for(let i = 1; i < n; i+=2){
+       let space = " ".repeat(topCount);
+       top += `${space}${`*`.repeat(i)}\n`;
+       topCount--;
+    }
+  
+    return top.concat(bottom)
+  }
