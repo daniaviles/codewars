b26be1954bf50b35d5c3d876b9c26f3a1b2deef4
Dani <danielle.a.aviles@gmail.com>
2023-03-22 09:58:33 -0400

    Added 8kyu coding challenge Double Char

---
+++ js/solution55.js
@@ -0,0 +1,17 @@
+// Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+// Examples (Input -> Output):
+
+// * "String"      -> "SSttrriinngg"
+// * "Hello World" -> "HHeelllloo  WWoorrlldd"
+// * "1234!_ "     -> "11223344!!__  "
+// Good Luck!
+
+function doubleChar(str) {
+  str = str.split("")
+  let newString = []
+  for(let i = 0; i < str.length; i++){
+    newString.push(str[i] + str[i])
+  }
+  return newString.join("");
+}
